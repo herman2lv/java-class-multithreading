b6a869df7c6de8fe8e3b6465fa995baa91b594a3
Herman Rabinkin <herman2lv@gmail.com>
2022-08-02 22:38:19 +0300

    Modify example with StringBuilder/Buffer

--- src/com/hrm/threads/stringbuffer/SBThreadsDemo.java
+++ src/com/hrm/threads/stringbuffer/SBThreadsDemo.java
@@ -2,24 +2,14 @@ package com.hrm.threads.stringbuffer;
 
 public class SBThreadsDemo {
     
-    private final static String longStringA = "a".repeat(150) + '\n';
-    private final static String longStringX = "X".repeat(150) + '\n';
+    private final static String longStringA = "Kandiboberization!\n";
+    private final static String longStringX = "BBBBBBBBBBBBBBBBBB\n";
     
     public static void main(String[] args) throws InterruptedException {
-        StringBuilder notSafe = new StringBuilder();
-//        StringBuffer safe = new StringBuffer();
-        Thread th1 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                notSafe.append(longStringA);
-//                safe.append(longStringA);
-            }
-        });
-        Thread th2 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                notSafe.append(longStringX);
-//                safe.append(longStringX);
-            }
-        });
+        StringBuilder builder = new StringBuilder();
+//        StringBuffer builder = new StringBuffer();
+        Thread th1 = new Thread(() -> perform(builder, longStringA));
+        Thread th2 = new Thread(() -> perform(builder, longStringX));
         
         th2.start();
         th1.start();
@@ -27,8 +17,14 @@ public class SBThreadsDemo {
         th1.join();
         th2.join();
         
-        System.out.println(notSafe.toString());
-//        System.out.println(safe.toString());
+        System.out.println(builder.toString());
+    }
+    
+    private static void perform(StringBuilder builder, String charsToAppend) {
+//    private static void perform(StringBuffer builder, String charsToAppend) {
+        for (int i = 0; i < 100; i++) {
+            builder.append(charsToAppend);
+        }
     }
 
 }
